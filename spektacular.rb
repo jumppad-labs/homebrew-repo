38ee8dd5999a2a176e95958bfb8a5c7ce42eda3f
Mr Jumppad <hello@jumppad.dev>
2026-06-04 09:27:32 +0100

    Update version to 0.7.0

--- spektacular.rb
+++ spektacular.rb
@@ -5,26 +5,26 @@
 class Jumppad < Formula
   desc ""
   homepage "https://jumppad.dev"
-  version "0.6.0"
+  version "0.7.0"
 
   if OS.mac? && Hardware::CPU.intel?
-    url "https://github.com/jumppad-labs/spektacular/releases/download/0.6.0/spektacular_0.6.0_darwin_x86_64.zip"
-    sha256 "b44b07fa006104b26b06ff5b82e8b72b272ed9d58f004a8b2c6b3f544a66f823"
+    url "https://github.com/jumppad-labs/spektacular/releases/download/0.7.0/spektacular_0.7.0_darwin_x86_64.zip"
+    sha256 "fc6de24c3f65cf0fd2f41dd05d3c9e7cfbc666610aeb47cfc3f6a19c4e5bc090"
   end
 
   if OS.mac? && Hardware::CPU.arm?
-    url "https://github.com/jumppad-labs/spektacular/releases/download/0.6.0/spektacular_0.6.0_darwin_arm64.zip"
-    sha256 "62e7356b253b49b50ebcf256ff0eac70c68a4986ac97d0fa592ecf022955e597"
+    url "https://github.com/jumppad-labs/spektacular/releases/download/0.7.0/spektacular_0.7.0_darwin_arm64.zip"
+    sha256 "60c4a1e8361f5d67cb8cdd32d2008bfd23bcce529cb5f6b94cb9d12aef712d7c"
   end
 
   if OS.linux? && Hardware::CPU.intel?
-    url "https://github.com/jumppad-labs/spektacular/releases/download/0.6.0/spektacular_0.6.0_linux_x86_64.tar.gz"
-    sha256 "b4f8bc389659d62dc1dd0217df5e2a0f784e2379e849451ca40d9bd47e107b34"
+    url "https://github.com/jumppad-labs/spektacular/releases/download/0.7.0/spektacular_0.7.0_linux_x86_64.tar.gz"
+    sha256 "99a2986660d5b3bcc5317dff7c5a698dacf2fba2576f40ddcf55b9477a28bb45"
   end
 
   if OS.linux? && Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-    url "https://github.com/jumppad-labs/spektacular/releases/download/0.6.0/spektacular_0.6.0_linux_arm64.tar.gz"
-    sha256 "44a6e5689ba68c72fd45461dfe8458802e675b582645746aff50f34e9186ca5e"
+    url "https://github.com/jumppad-labs/spektacular/releases/download/0.7.0/spektacular_0.7.0_linux_arm64.tar.gz"
+    sha256 "392752d3f61ae45069edcfb71850f891c64f0050366892ce77420385804f6417"
   end
 
   def install
