3457a7710b973aeed8b29dad826a0151bfdab60c
Mr Jumppad <hello@jumppad.dev>
2026-06-10 10:50:08 +0100

    Update version to 0.8.0

--- spektacular.rb
+++ spektacular.rb
@@ -5,26 +5,26 @@
 class Jumppad < Formula
   desc ""
   homepage "https://jumppad.dev"
-  version "0.7.0"
+  version "0.8.0"
 
   if OS.mac? && Hardware::CPU.intel?
-    url "https://github.com/jumppad-labs/spektacular/releases/download/0.7.0/spektacular_0.7.0_darwin_x86_64.zip"
-    sha256 "fc6de24c3f65cf0fd2f41dd05d3c9e7cfbc666610aeb47cfc3f6a19c4e5bc090"
+    url "https://github.com/jumppad-labs/spektacular/releases/download/0.8.0/spektacular_0.8.0_darwin_x86_64.zip"
+    sha256 "961044e12bc86afe9062e0e49e9fbf8cd0e3ce2f442e7b9b5337bdd85e4782cf"
   end
 
   if OS.mac? && Hardware::CPU.arm?
-    url "https://github.com/jumppad-labs/spektacular/releases/download/0.7.0/spektacular_0.7.0_darwin_arm64.zip"
-    sha256 "60c4a1e8361f5d67cb8cdd32d2008bfd23bcce529cb5f6b94cb9d12aef712d7c"
+    url "https://github.com/jumppad-labs/spektacular/releases/download/0.8.0/spektacular_0.8.0_darwin_arm64.zip"
+    sha256 "0f732f742996f9a803cb3c98898dd8fd4f1dc64f1813303e885c77844924d443"
   end
 
   if OS.linux? && Hardware::CPU.intel?
-    url "https://github.com/jumppad-labs/spektacular/releases/download/0.7.0/spektacular_0.7.0_linux_x86_64.tar.gz"
-    sha256 "99a2986660d5b3bcc5317dff7c5a698dacf2fba2576f40ddcf55b9477a28bb45"
+    url "https://github.com/jumppad-labs/spektacular/releases/download/0.8.0/spektacular_0.8.0_linux_x86_64.tar.gz"
+    sha256 "4d59095bd5bb09db317f7e78af922b77ebb32bdcaf1dfe042ac814f189c16fd8"
   end
 
   if OS.linux? && Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-    url "https://github.com/jumppad-labs/spektacular/releases/download/0.7.0/spektacular_0.7.0_linux_arm64.tar.gz"
-    sha256 "392752d3f61ae45069edcfb71850f891c64f0050366892ce77420385804f6417"
+    url "https://github.com/jumppad-labs/spektacular/releases/download/0.8.0/spektacular_0.8.0_linux_arm64.tar.gz"
+    sha256 "dcad25c5603eb5527488c96a44e8bc4eb8a24ab8600adff8df63e2826075e1f2"
   end
 
   def install
