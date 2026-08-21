346a505d53e079c1861e955c73cce54fbd7d284d
Mr Jumppad <hello@jumppad.dev>
2026-08-21 16:52:02 +0100

    Update version to 0.18.2

--- spektacular.rb
+++ spektacular.rb
@@ -5,26 +5,26 @@
 class Spektacular < Formula
   desc ""
   homepage "https://jumppad.dev"
-  version "0.18.1"
+  version "0.18.2"
 
   if OS.mac? && Hardware::CPU.intel?
-    url "https://github.com/jumppad-labs/spektacular/releases/download/0.18.1/spektacular_0.18.1_darwin_x86_64.zip"
-    sha256 "749709ee5854edef485ce8c66ee9a1cd99cf44ed22b93e1a1f5e262c2eb87d2e"
+    url "https://github.com/jumppad-labs/spektacular/releases/download/0.18.2/spektacular_0.18.2_darwin_x86_64.zip"
+    sha256 "ca5f7870940e7e6898a17a93794d9f8b60a4b766c367e72196f56f48a75bdc0c"
   end
 
   if OS.mac? && Hardware::CPU.arm?
-    url "https://github.com/jumppad-labs/spektacular/releases/download/0.18.1/spektacular_0.18.1_darwin_arm64.zip"
-    sha256 "8fd0626776adf822eaf287fe2fd8d204dc55d207bbf346723ca67bec9c539e98"
+    url "https://github.com/jumppad-labs/spektacular/releases/download/0.18.2/spektacular_0.18.2_darwin_arm64.zip"
+    sha256 "d0efa8f19af3319053eb18aacefcdcf290fb33f7aaac20385dae49955839a44d"
   end
 
   if OS.linux? && Hardware::CPU.intel?
-    url "https://github.com/jumppad-labs/spektacular/releases/download/0.18.1/spektacular_0.18.1_linux_x86_64.tar.gz"
-    sha256 "053f6bf05e4915cf0c0051c4e7e069040d338134dbedae90d7ad358ddf1fe6a8"
+    url "https://github.com/jumppad-labs/spektacular/releases/download/0.18.2/spektacular_0.18.2_linux_x86_64.tar.gz"
+    sha256 "030e5843db8b9c6c48506af0c16724a10a11ac507989693c35d6f5b944a759d0"
   end
 
   if OS.linux? && Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-    url "https://github.com/jumppad-labs/spektacular/releases/download/0.18.1/spektacular_0.18.1_linux_arm64.tar.gz"
-    sha256 "f1744067d1f3db8a6b4a1f020bf82fa5cb4c30b6829716d4c7619c31bd60a13d"
+    url "https://github.com/jumppad-labs/spektacular/releases/download/0.18.2/spektacular_0.18.2_linux_arm64.tar.gz"
+    sha256 "9d355172a188c6d9ef5ff3a0df049078bd6433154100420c299d6abd30429bab"
   end
 
   def install
