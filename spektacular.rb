0880c23b7b29b84d394e4b20eca9a6cf3feb3099
Mr Jumppad <hello@jumppad.dev>
2026-09-09 20:39:00 +0100

    Update version to 0.18.4

--- spektacular.rb
+++ spektacular.rb
@@ -5,26 +5,26 @@
 class Spektacular < Formula
   desc ""
   homepage "https://jumppad.dev"
-  version "0.18.3"
+  version "0.18.4"
 
   if OS.mac? && Hardware::CPU.intel?
-    url "https://github.com/jumppad-labs/spektacular/releases/download/0.18.3/spektacular_0.18.3_darwin_x86_64.zip"
-    sha256 "60bc5ded38ab89c5f658d87cee1fed8b1337f27d1f1b348fa6142e465262238d"
+    url "https://github.com/jumppad-labs/spektacular/releases/download/0.18.4/spektacular_0.18.4_darwin_x86_64.zip"
+    sha256 "71daccdd29612364c34f3c110a15d6e2a47adb51642c433a2b62ee43da42ea6c"
   end
 
   if OS.mac? && Hardware::CPU.arm?
-    url "https://github.com/jumppad-labs/spektacular/releases/download/0.18.3/spektacular_0.18.3_darwin_arm64.zip"
-    sha256 "383c8b24bd3a6b59bfc45b32aff8775114985c97b3e8013e5c4f46e06d08d195"
+    url "https://github.com/jumppad-labs/spektacular/releases/download/0.18.4/spektacular_0.18.4_darwin_arm64.zip"
+    sha256 "db127c18d8b98835c207860287d30a157211e3b8d09d24d26c6073fa3b082cc8"
   end
 
   if OS.linux? && Hardware::CPU.intel?
-    url "https://github.com/jumppad-labs/spektacular/releases/download/0.18.3/spektacular_0.18.3_linux_x86_64.tar.gz"
-    sha256 "6abf6267174e614af01e90713c37b7b5efee80a88e2a8a742a387bd146d1b528"
+    url "https://github.com/jumppad-labs/spektacular/releases/download/0.18.4/spektacular_0.18.4_linux_x86_64.tar.gz"
+    sha256 "c1196783459c6ede4d91c530d15cb575aca67b78075ea3c28cf81c5b6691f857"
   end
 
   if OS.linux? && Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-    url "https://github.com/jumppad-labs/spektacular/releases/download/0.18.3/spektacular_0.18.3_linux_arm64.tar.gz"
-    sha256 "d9954966798c630f2437d0531089fb37d89780d592b6760ded78be23b67f9bc8"
+    url "https://github.com/jumppad-labs/spektacular/releases/download/0.18.4/spektacular_0.18.4_linux_arm64.tar.gz"
+    sha256 "5c8b695a0239dff95baa7954a67b3f194d2d256daf3b76be5b2f552468242d3e"
   end
 
   def install
